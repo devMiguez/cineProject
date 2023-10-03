ed3e780bac95ce00fb74c16c974c090ded0fbab0
joao <joaovmiguez@gmail.com>
2023-10-03 14:15:17 -0300

    First commit

---
+++ cinema/src/main/java/com/cinema/cinema/CinemaApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+	}
+
+}
